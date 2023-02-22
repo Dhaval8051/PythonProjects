38759e5d89e8ce61ee73cef337783e8492f37df1
Dhaval Khatri <Dhaval.Khatri@einfochips.com>
2023-02-22 11:52:25 +0530

    Added for loop

---
+++ 22_Feb/datatypes.py
@@ -0,0 +1,11 @@
+#for a in range(1,11):
+   # print(a)
+
+#colors = ["red", "green",]"yellow", "blue","green","black","grey"]
+
+#or i in range (0, len(colors)):
+    #print(colors[i])
+numbers=[45,98,75,65,24,88,74,56,75]
+for i in range(0,len(numbers)):
+    if numbers[i]<=50:
+        print(numbers[i])
